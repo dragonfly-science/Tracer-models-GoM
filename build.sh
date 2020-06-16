3095b4b6ebf439332a3df18ccdaa8dfc42bc694f
Philipp-Neubauer <neubauer.phil@gmail.com>
2020-06-17 11:52:38 +1200

    s3 storage for GoM

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ Rscript -e "rmarkdown::render('analysis.Rmd')"
 cp *.html /output/
 cp *.Rdata /output/
 cp *.html /publish/
+
+s3cmd -v \
+      --access_key=${AWS_ACCESS_KEY_ID} \
+      --secret_key=${AWS_SECRET_ACCESS_KEY} \
+      sync -r ./GoM_tracers.Rdata s3://${AWS_BUCKET}/
--- post-proc.R
+++ post-proc.R
@@ -1,3 +1,13 @@
+#### Setup #####
+
+require(dplyr)
+require(aws.s3)
+
+s3load('GoM_tracers.Rdata', 
+       bucket = 'gom-diets',
+       key = 'AKIARYTPAP6R2SC2FEOT',
+       secret = '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')
+
 diet_props <- lapply(modlist, function(l) {
   props <- rstan::extract(l$mod,pars='prop')$prop
   colnames(props) <- l$prey
@@ -15,4 +25,18 @@ draw_diets <- function(diets, predator, nsamples){
   tidyr::pivot_wider(out, names_from = Prey, values_from = value)[,-c(1,2)]
 }
 
-draw_diets(diet_props, 'Atlantic Croaker PS', 1000)
+
+#############################################
+############ Now draw diet proportions   ####
+#############################################
+
+
+# Note, each line is 1 sample = sums to 1
+draw_diets(diet_props, 'Blue crab PS',        nsamples = 100)
+draw_diets(diet_props, 'Killifish PS',        nsamples = 100)
+draw_diets(diet_props, 'Atlantic Croaker PS', nsamples = 100)
+
+draw_diets(diet_props, 'Blue crab WPH',        nsamples = 100)
+draw_diets(diet_props, 'Killifish WPH',        nsamples = 100)
+draw_diets(diet_props, 'Atlantic Croaker WPH', nsamples = 100)
+
